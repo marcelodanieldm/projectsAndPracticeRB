90bc821050ad0e17c1cc594f22ef59753c83b8dc
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-25 23:42:06 -0300

    Update BusBooking.rb

--- BusBooking.rb
+++ BusBooking.rb
@@ -1,1 +1,17 @@
 
+//creating classes bus and menu.
+  
+class Bus
+
+end
+
+class Menu
+  def mostrar_menu
+    ---[[Eldani Tours]]---
+      1)Enter a passenger
+      2)Clear a passenger
+      3)Show bus
+      4)Exit
+  
+end
+
