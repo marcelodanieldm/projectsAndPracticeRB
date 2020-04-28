7d9a28e58b706464563e6c10ba105ba7d343ad4a
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-27 22:07:06 -0300

    Create ejerciciosVarios3.rb

---
+++ Ejercicios/ejerciciosVarios3.rb
@@ -0,0 +1,11 @@
+#cargar dos enteros y hacer una simple suma.
+
+puts "ingresar el primer numero"
+numero1 = gets.to_i
+
+puts "ingresar el segundo numero"
+numero2 = gets.to_i
+
+resultado = numero1 + numero2
+
+puts "el resultado es: #{resultado} "
