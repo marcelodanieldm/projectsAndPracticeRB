d8123f117755544c1f963b7a241c872d80a814d3
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-27 21:53:31 -0300

    Create ejerciciosVarios2.rb

---
+++ Ejercicios/ejerciciosVarios2.rb
@@ -0,0 +1,13 @@
+=begin
+
+Calcular el sueldo mensual de un operario conociendo la cantidad de horas trabajadas y el pago por hora.
+
+=end
+
+puts "Ingrese las horas trabajadas por el empleado"
+horas_trabajadas = gets.to_i
+puts "Ingrese el pago por hora"
+costo_hora = gets.to_f
+sueldo = horas_trabajadas * costo_hora
+puts "El sueldo total del operario es "
+puts sueldo
