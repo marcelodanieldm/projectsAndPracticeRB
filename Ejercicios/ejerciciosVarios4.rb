e808a2bcb7eb5737d6148e8b21edbb97d0734d6c
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-27 22:11:41 -0300

    Create ejerciciosVarios4.rb

---
+++ Ejercicios/ejerciciosVarios4.rb
@@ -0,0 +1,6 @@
+=begin
+Realizar la carga del lado de un cuadrado,
+mostrar por pantalla el perímetro del mismo 
+(El perímetro de un cuadrado se calcula multiplicando el valor del lado por cuatro)
+=end
+
