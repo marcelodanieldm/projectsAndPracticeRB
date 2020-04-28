66cca9df3e08e5f9923a2a08cee03ff1e7500d49
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-27 22:14:23 -0300

    Update ejerciciosVarios4.rb

--- Ejercicios/ejerciciosVarios4.rb
+++ Ejercicios/ejerciciosVarios4.rb
@@ -4,3 +4,9 @@ mostrar por pantalla el perímetro del mismo
 (El perímetro de un cuadrado se calcula multiplicando el valor del lado por cuatro)
 =end
 
+lado1= gets.to_i
+lado2= gets.to_i
+perimetro= lado1*lado2
+puts = "el perimetro del cuadrado es: #{perimetro}"
+
+
