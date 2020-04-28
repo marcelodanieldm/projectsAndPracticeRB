3ac754eb8be7fca43a67c98732dcd277ae5b5d3c
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-27 22:18:51 -0300

    Update ejerciciosvarios5.rb

--- Ejercicios/ejerciciosvarios5.rb
+++ Ejercicios/ejerciciosvarios5.rb
@@ -1,1 +1,10 @@
+#Se debe desarrollar un programa que pida el ingreso del precio de un artículo y la cantidad que lleva el cliente.
+#Mostrar lo que debe abonar el comprador.
 
+puts "Ingrese la cantidad de articulos a llevar"
+cantidad = gets.to_i
+puts "Ingrese el valor unitario del producto"
+precio = gets.to_f
+importe = precio * cantidad
+puts "El importe a pagar es"
+puts importe
